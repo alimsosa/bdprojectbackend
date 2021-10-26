1035b431b0b2c1a30bb2f687984f9b112b2fdb04
alimsosa <42209713+alimsosa@users.noreply.github.com>
2021-10-26 20:02:02 -0300

    Add files via upload

---
+++ src/test/java/com/bdproject/Bases/Proyecto/BasesProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdproject.Bases.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasesProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
